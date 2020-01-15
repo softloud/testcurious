a71f3bbfd5a1fdd962da3de5fbd06102fd6a167f
Charles T. Gray <charlestigray@gmail.com>
2020-01-15 23:02:04 +1100

    #1 forgot to build site

--- docs/tests/testthat/test-neet-duck.R
+++ docs/tests/testthat/test-neet-duck.R
@@ -1,6 +1,11 @@
 context("test that the duck quacks")
 
-expect_neet_quack <- function() {
-
-
+expect_neet_quack <- function(quack_output) {
+  expect_gte(stringr::str_length(quack_output), 1)
+  expect_type(quack_output, "character")
 }
+
+test_that("duck quacks", {
+  expect_neet_quack(quack())
+  expect_neet_quack(quack("quack, quack!"))
+})
